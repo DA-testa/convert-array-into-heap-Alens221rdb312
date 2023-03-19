e7ebd64ecfa7dbf7a16f8141e75e92e794c20926
Alens221rdb312 <125511344+Alens221rdb312@users.noreply.github.com>
2023-03-19 10:42:22 +0000

    test1

--- build_heap.py
+++ build_heap.py
@@ -3,9 +3,35 @@
 
 def build_heap(data):
     swaps = []
+    n = len(data)
+
+    for i in range(n//2 - 1, -1, -1):
+        swaps += sift_down(i, data)
+
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    return swaps
+
+def sift_down(i, data):
+    swaps = []
+    n = len(data)
+
+    min_child = i
+    left_child = 2*i + 1
+    right_child = 2*i + 2
+
+    if left_child < n and data[left_child] < data[min_child]:
+        min_child = left_child
+
+    if right_child < n and data[right_child] < data[min_child]:
+        min_child = right_child
+
+    if i != min_child:
+        swaps.append((i, min_child))
+        data[i], data[min_child] = data[min_child], data[i]
+
+        swaps += sift_down(min_child, data)
 
     return swaps
 
@@ -40,3 +66,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Alens Ilgavižs 221RDB312 4.g
